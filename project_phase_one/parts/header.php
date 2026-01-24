f6576f0100f75b63fe7b883aed91a00055b9945a
Thiemi Soubhia Doi <99260862+thiemisoubhia@users.noreply.github.com>
2026-01-24 12:31:37 -0500

    font-family

--- project_phase_one/parts/header.php
+++ project_phase_one/parts/header.php
@@ -34,9 +34,9 @@
                 <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
                     <div class="navbar-nav">
                         <a class="nav-item nav-link active" href="#">Home</a>
-                        <a class="nav-item nav-link text-light" href="#">Tips</a>
-                        <a class="nav-item nav-link text-light" href="#">Create your resume</a>
-                        <a class="nav-item nav-link text-light" href="#">Feedback</a>
+                        <a class="nav-item nav-link" href="#">Tips</a>
+                        <a class="nav-item nav-link" href="#">Create your resume</a>
+                        <a class="nav-item nav-link" href="#">Feedback</a>
                     </div>
                 </div>
             </nav>
