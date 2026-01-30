7f672e0687156bef260c67d5abcdab3c928c27ce
Thiemi Soubhia Doi <99260862+thiemisoubhia@users.noreply.github.com>
2026-01-30 14:36:13 -0500

    show the message when sent

--- assignments/labs/lab_three/includes/mail.php
+++ assignments/labs/lab_three/includes/mail.php
@@ -1,5 +1,6 @@
 <?php require "includes/header.php";
 
+//getting the data from the form
 $firstname = filter_input(INPUT_POST, 'first_name', FILTER_SANITIZE_SPECIAL_CHARS);
 $lastname = filter_input(INPUT_POST, 'last_name', FILTER_SANITIZE_SPECIAL_CHARS);
 $message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_SPECIAL_CHARS);
@@ -26,22 +27,30 @@ if ($message === null || $message === '') {
 // email validation
 if ($email === null || $email === '') {
     $errors[] = "Email is required.";
-} 
+}
 // email format validation
 elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
     $errors[] = "Email must be a valid email";
 }
 
-
+//checking the errors
+if (!empty($errors)) {
+    echo "<ul>";
+    foreach ($errors as $error) {
+        echo "<li>$error</li>";
+    }
+    echo "</ul>";
+} 
+//show message when sent
+else{
 ?>
-
 <main>
 
     <?php echo "<h2>Message sent! Thank for your message " . $firstname . "!</h2>" ?>
-    <h3>Our team are going to reach you out in 2 bussines days</h3>
+    <h3>Our team will reach out within 2 business days</h3>
 </main>
-
+<?php }?>
 <!-- send email using mail function -->
- <!-- mail($to, $subject, $message); -->
+<!-- mail($to, $subject, $message); -->
 
 <?php require "includes/footer.php"; ?>
