843a3c6b31626a419f3aae99a4098a5873006692
Thiemi Soubhia Doi <99260862+thiemisoubhia@users.noreply.github.com>
2026-02-06 12:16:17 -0500

    lab_4 initial commit

--- project_phase_one/parts/footer.php
+++ project_phase_one/parts/footer.php
@@ -4,7 +4,6 @@
         <p class="text-light">&copy; 2026 Thiemi Soubhia Doi. All rights reserved.</p>
     </div>
 </footer>
-</footer>
 
 <!-- including boostrap JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
