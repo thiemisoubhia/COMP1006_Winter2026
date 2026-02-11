d543033c92835a049e4c5e77e27d6c84511155eb
Thiemi Soubhia Doi <99260862+thiemisoubhia@users.noreply.github.com>
2026-02-11 11:33:26 -0500

    project comment

--- project_phase_one/index.php
+++ project_phase_one/index.php
@@ -1,6 +1,15 @@
 <!-- started on January 24th, 2026 -->
-<!-- this project was created using Bootstrap 5.0 official documentation: https://getbootstrap.com/docs/5.0/ -->
+<!-- project created using Bootstrap 5.0 official documentation: https://getbootstrap.com/docs/5.0/ -->
 <?php
+
+    /*This project was a great way to practice everything I’ve learned in class. 
+    I got to work with forms, validate and sanitize input in PHP, and save data securely using PDO and phpAdmin. 
+    I also practiced organizing my code with includes for headers, footers 
+    and styling everything nicely with bootstrap. 
+    Overall, it helped me understand how to build a web application with PHP, HTML and SQL.*/
+
+
+
 // header and navbar
 require "parts/header.php";
 
