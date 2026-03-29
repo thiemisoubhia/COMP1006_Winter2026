d65338f48592a2a9ee96e5543660e209f3778935
Thiemi Soubhia Doi <99260862+thiemisoubhia@users.noreply.github.com>
2026-03-29 17:26:55 -0400

    new table resumes

--- assignments/SQL/resumes.sql
+++ assignments/SQL/resumes.sql
@@ -1,10 +1,13 @@
+/*new resume table*/
 CREATE TABLE resumes (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(100) NOT NULL,
-    last_name  VARCHAR(100) NOT NULL,
-    position   VARCHAR(150) NOT NULL,
-    skills     TEXT NOT NULL,
-    email      VARCHAR(150) NOT NULL,
-    phone      VARCHAR(50) NOT NULL,
-    bio        TEXT NOT NULL
+    last_name VARCHAR(100) NOT NULL,
+    position VARCHAR(150) NOT NULL,
+    skills TEXT NOT NULL,
+    email VARCHAR(150) NOT NULL,
+    phone VARCHAR(50) NOT NULL,
+    bio TEXT NOT NULL,
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
