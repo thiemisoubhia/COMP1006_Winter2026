614df0bb6f83659786c1affd1aa5970c1aa2f42f
Thiemi Soubhia Doi <99260862+thiemisoubhia@users.noreply.github.com>
2026-03-29 21:45:27 -0400

    New authentication message

--- project_phase_two/protect.php
+++ project_phase_two/protect.php
@@ -6,7 +6,7 @@ require_once 'parts/header.php';
 <body>
     <main class="container text-center mt-5">
         <!--message for users who are not logged in -->
-        <h3>Sorry, you must be logged in to create your ResuME!</h3>
+        <h3 class="pt-2">Sorry, you must be logged in to create your ResuME!</h3>
         
         <!-- button to login -->
         <a href="login.php" class="btn btn-dark mt-3">Login</a>
