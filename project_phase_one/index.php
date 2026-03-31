8baa0bd3f2dcef76bacee0310e661265764adfde
Thiemi Soubhia Doi <99260862+thiemisoubhia@users.noreply.github.com>
2026-03-31 15:40:21 -0400

    Reflection phase 02

--- project_phase_one/index.php
+++ project_phase_one/index.php
@@ -2,14 +2,6 @@
 <!-- project created using Bootstrap 5.0 official documentation: https://getbootstrap.com/docs/5.0/ -->
 <?php
 
-    /*This project was a great way to practice everything I’ve learned in class. 
-    I got to work with forms, validate and sanitize input in PHP, and save data securely using PDO and phpAdmin. 
-    I also practiced organizing my code with includes for headers, footers 
-    and styling everything nicely with bootstrap. 
-    Overall, it helped me understand how to build a web application with php, html and sql.*/
-
-
-
 // header and navbar
 require "parts/header.php";
 
--- project_phase_two/parts/reflection.php
+++ project_phase_two/parts/reflection.php
@@ -1,17 +1,27 @@
-    <div id="feedback" class="py-5">
+    <div id="reflection" class="py-5">
         <div class="container">
+            <h2>Phase 1 Reflection</h2>
+            <p> This project was a great way to practice everything I’ve learned in class.
+                I got to work with forms, validate and sanitize input in PHP, and save data securely using PDO and phpAdmin.
+                I also practiced organizing my code with includes for headers, footers
+                and styling everything nicely with bootstrap.
+                Overall, it helped me understand how to build a web application with php, html and sql.</p>
+
             <h2>Phase 2 Reflection</h2>
             <p>This phase was all about making the app more real by adding user accounts and security.
                 The hardest part was getting login, registration, and reCAPTCHA to work correctly
                 together and making sure only logged-in users could see certain pages.
             </p>
-            <p>I’m happy I got the registration, login, logout, and resume creation working, 
-and users can now upload files safely. It feels good to see the app actually secure 
-and interactive compared to Phase 1.
-</p>
-<p>I learned a lot about sessions, password hashing, and checking user input properly. 
-Also learned how to organize PHP pages with header/footer parts to keep things clean.
-</p>
+            <p>I’m happy I got the registration, login, logout, and resume creation working,
+                and users can now upload files safely. It feels good to see the app actually secure
+                and interactive compared to Phase 1.
+            </p>
+            <p>I learned a lot about sessions, password hashing, and checking user input properly.
+                Also learned how to organize PHP pages with header/footer parts to keep things clean.
+            </p>
+            <p>Next Step:
+                It would be really good to have an admin area to see all users and their resumes.
+                 I also want to connect the app to other APIs to add more features, like sending emails or fetching extra data.</p>
 
 
         </div>
